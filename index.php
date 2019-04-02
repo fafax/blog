32e990bcc018bb310560035d263a65abb0f0b762
fafax <fabienhamayon@free.fr>
2019-04-02 18:55:40 +0200

    add files home.twig

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    test
-</body>
-</html>
+<?php
+
+require_once "./controller/home.php";
