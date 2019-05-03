6f311bc1091a1156ac99a6bf77db9be204fa828e
fabien hamayon <fabienhamayon@free.fr>
2019-05-03 16:28:29 +0200

    add first and last name for create post

--- model/PostManager.php
+++ model/PostManager.php
@@ -7,7 +7,7 @@ class PostManager
    public function getAllPost(){
       $bdd = new Connection();
       $bd = $bdd->get_bd();
-      $req = $bd->prepare('SELECT * from post ORDER BY  id_post DESC');
+      $req = $bd->prepare('SELECT id_post,title,lede,url_image,post.create_date, first_name ,last_name FROM post, user WHERE User_id_user = id_user ORDER BY id_post DESC');
       $req->execute();
       $data = $req->fetchAll(PDO::FETCH_OBJ);
       return $data;
