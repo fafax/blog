987f074a73855c789b52055188ced4db0777b3bc
fabien hamayon <fabienhamayon@free.fr>
2019-06-30 09:39:02 +0200

    refactoring checkAuthentification function

--- model/Authentificate.php
+++ model/Authentificate.php
@@ -4,44 +4,47 @@ namespace App;
 
 class Authentificate
 {
-    public function find(int $id): UserEntity
-    {
-        $bdd = new Connexion();
-        $response = $bdd->getBd()->prepare('SELECT * FROM User WHERE id_user = :id');
-        $response->bindValue(':id', $id);
-        $response->execute();
-
-        return $response->fetchObject('App\UserEntity');
-    }
-
-    public function findByEmail(string $email): array
+    public function findByEmail(string $email): UserEntity
     {
         $bdd = new Connexion();
         $response = $bdd->getBd()->prepare('SELECT * FROM User WHERE email = :email');
         $response->bindValue(':email', $email);
         $response->execute();
 
-        return $response->fetch();
+        return $response->fetchObject('App\UserEntity');
+    }
+
+    public function checkPassword(string $email, string $password, UserEntity $user): bool
+    {
+        if (password_verify($password, $user->getPassword()) && $email == $user->getEmail()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function checkAdmin(int $userId)
+    {
+        if ($userId === 1) {
+            $_SESSION['admin'] = true;
+        }
     }
 
     public function checkAuthentification(string $email, string $password): bool
     {
-        if ($result = $this->findByEmail($email)) {
-            $user = $this->find($result['id_user']);
-            if (password_verify($password, $result['password']) && $email == $user->getEmail()) {
-                $_SESSION['id'] = $user->getIdUser();
-                if ($user->getRoleIdRole() === 1) {
-                    // if id and mdp and (id_role is egale adiminstrator) is correct then connect session and show link administration
-                    $_SESSION['admin'] = true;
-                }
-                // if id and mdp is correct then connect session
-                $_SESSION['connect'] = true;
+        $user = $this->findByEmail($email);
+        if ($this->checkPassword($email, $password, $user)) {
+            $_SESSION['id'] = $user->getIdUser();
 
-                return true;
-            }
+            $this->checkAdmin($user->getRoleIdRole());
+
+            $_SESSION['connect'] = true;
+
+            return true;
+        } else {
+            $_SESSION['id'] = 'noConnect';
+
+            return false;
         }
-        $_SESSION['id'] = 'noConnect';
-
-        return false;
     }
 }
